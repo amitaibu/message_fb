4d9f27b5eddee7572deacd1721d009857323cb5d
Amitai Burstein <amitai@gizra.com>
2013-04-06 16:56:55 +0300

    More work.

--- theme/message-fb.tpl.php
+++ theme/message-fb.tpl.php
@@ -10,21 +10,28 @@
     </h2>
   <?php endif; ?>
 
-  <div class="message-content"<?php print $content_attributes; ?>>
-    <?php
+  <div class="col-first">
+    <?php print $content['picture']; ?>
+  </div>
+
+  <div class="col-last">
+    <div class="message-content"<?php print $content_attributes; ?>>
+      <?php
       hide($content[$babble_field]);
       hide($content[$babble_field . '_babble_comment_form']);
       hide($content[$babble_field . '_babble_host_links']);
       print render($content);
-    ?>
-  </div>
+      ?>
+    </div>
 
-  <div class="message-comment">
-    <?php
+    <div class="message-comment">
+      <?php
       print render($content[$babble_field]);
       print render($content[$babble_field . '_babble_comment_form']);
       print render($content[$babble_field . '_babble_host_links']);
 
-    ?>
+      ?>
+    </div>
   </div>
+
 </div>
